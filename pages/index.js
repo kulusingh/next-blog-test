f1c8eaa1079e83bc89ee28094cef51443e413608
kulu singh <kulusingh@gmail.com>
2020-05-16 13:34:08 +0000

    files

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
+import React from 'react'
 import Head from 'next/head'
+import Link from 'next/link'
 
-export default function Home() {
-  return (
+const Home = () => (
     <div className="container">
       <Head>
         <title>Create Next App</title>
@@ -10,7 +11,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to This is home page
         </h1>
 
         <p className="description">
@@ -19,31 +20,12 @@ export default function Home() {
 
         <div className="grid">
           <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
+            <h3>Welcome</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href="https://nextjs.org/docs" className="card">
+            <h3>About us</h3>
+            <p><Link href='/about'>go to about</Link></p>
           </a>
         </div>
       </main>
@@ -206,4 +188,5 @@ export default function Home() {
       `}</style>
     </div>
   )
-}
+
+  export default Home
